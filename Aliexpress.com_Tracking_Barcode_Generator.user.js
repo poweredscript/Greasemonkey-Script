767772c68b35dacaea09365ac6e9dfd694d010f7
poweredscript <poweredscript@gmail.com>
2017-06-25 06:47:08 +0700

    Update Aliexpress.com_Tracking_Barcode_Generator.user.js

--- Aliexpress.com_Tracking_Barcode_Generator.user.js
+++ Aliexpress.com_Tracking_Barcode_Generator.user.js
@@ -6,7 +6,7 @@
 // @author  	            Apichai Pashaiam
 // @downloadURL     https://github.com/poweredscript/Greasemonkey-Script/raw/master/Aliexpress.com_Tracking_Barcode_Generator.user.js
 // @updateURL 	    https://github.com/poweredscript/Greasemonkey-Script/raw/master/Aliexpress.com_Tracking_Barcode_Generator.user.js
-// @version             1.3
+// @version             1.4
 // @license             Apache
 // @include             *trade.aliexpress.com/order*
 // @require             https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
@@ -68,35 +68,49 @@ window.addEventListener("load", pageFullyLoaded);
 function pageFullyLoaded() {
     //http://track.aliexpress.com/logisticsdetail.htm?tradeId=80319479301398
     var orderActions = document.getElementsByClassName("order-action");
+    //alert(orderActions.length);
     for (var i = 0; i < orderActions.length; i++) {
         var orderAction = orderActions[i];
         var reAddToCarts = orderAction.getElementsByClassName("ui-button ui-button-normal button-reAddToCart");
+        //alert(reAddToCarts.length);break;
         if (reAddToCarts.length == 0) {
             var trackings = orderAction.getElementsByClassName("ui-button ui-button-normal button-logisticsTracking");
+            //alert(trackings.length);if(i > 5) break;
             for (var j = 0; j < trackings.length; j++) {
                 var eleTracking = trackings[j];
                 var trackingId = eleTracking.getAttribute("orderid");
+                //alert(trackingId);
                 if (trackingId != "") {
                     resourceText('http://track.aliexpress.com/logisticsdetail.htm?tradeId=' + trackingId, eleTracking, function (htmlXRates, eleTrackingOut) {
-                        var result = htmlXRates.match(/Tracking number:<\/div>[\s\S]*?<\/div>[\s\S]*?<\/div>/g)[0];
-                        result = result.match(/<div class="item msg">.*?<\/div>/g)[0];
-                        result = result.replace(/(<div class="item msg">|<\/div>)/g, "");
-                     
-                        if (result.length > 5) {
-                            var eleTrackingParent = eleTrackingOut.parentElement;
-                            var img = document.createElement("img");
-                            if (qrCode) {
-                                img.src = "http://www.qr-code-generator.com/phpqrcode/getCode.php?cht=qr&chs=150x150&choe=UTF-8&chld=L|0&chl=" + result;
-                                img.style.width = "150px";
-                                img.style.height = "150px";
-                            } else {
-                                img.src = "http://www.barcode-generator.org/zint/api.php?bc_number=20&bc_data=" + result;
-                                img.style.height = "95px";
+                        //alert(htmlXRates);
+                        //var matchs = htmlXRates.match(/Tracking number:<\/div>[\s\S]*?<\/div>[\s\S]*?<\/div>/g);
+                        var matchs = htmlXRates.match(/logisticsNo":".*?"/g);
+                        //logisticsNo":"06170928320"
+                        //alert(matchs.length)
+                        if (matchs.length > 0) {
+                            var result = matchs[0];
+                            result = result.replace(/(logisticsNo":"|")/g, "");
+                            //alert(result);
+                            //result = result.match(/<div class="item msg">.*?<\/div>/g)[0];
+                            //result = result.replace(/(<div class="item msg">|<\/div>)/g, "");
+
+                            if (result.length > 5) {
+                                var eleTrackingParent = eleTrackingOut.parentElement;
+                                var img = document.createElement("img");
+                                if (qrCode) {
+                                    img.src = "http://www.qr-code-generator.com/phpqrcode/getCode.php?cht=qr&chs=150x150&choe=UTF-8&chld=L|0&chl=" + result;
+                                    img.style.width = "150px";
+                                    img.style.height = "150px";
+                                } else {
+                                    img.src = "http://www.barcode-generator.org/zint/api.php?bc_number=20&bc_data=" + result;
+                                    img.style.height = "95px";
+                                }
+                                img.alt = result;
+                                img.title = result;
+                                eleTrackingParent.appendChild(img);
                             }
-                            img.alt = result;
-                            img.title = result;
-                            eleTrackingParent.appendChild(img);
                         }
+                       
                     });
                 }
             }

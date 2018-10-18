24ce3acf59f280eaf7bb22e5662c61ea987bfbaa
poweredscript <poweredscript@gmail.com>
2018-10-18 18:53:38 +0700

    Update Search_on_Stores.js

--- Search_on_Stores.js
+++ Search_on_Stores.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name           ♥ Search on Stores
+// @name           Search on Stores
 // @description    Search Selected text on Stores
 // @copyright      Mahi Balan M | RCA | VBI
 // @version        1.0
@@ -9,13 +9,7 @@
 // @author        Apichai P.
 // @grant         GM_openInTab
 // @run-at        context-menu
-// @require       http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
-// ==/UserScript==
-
-// GM_registerMenuCommand('Run this now', function() {
-//     alert("Put script's main function here");
-// }, 'r');
 
 
 (function() {
@@ -36,7 +30,7 @@ function getSelectionText() {
         var txt = getSelectionText();
         if(txt.length > 0)
         {
-            GM_openInTab("https://www.aliexpress.com/af/usb.html?SearchText=" + encodeURIComponent(txt) + "&SortType=price_asc", true);
+            GM_openInTab("https://www.aliexpress.com/af/usb.html?SearchText=" + encodeURIComponent(txt), true);// + "&SortType=price_asc"
             GM_openInTab("https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&LH_BIN=1&_sop=15&_nkw=" + encodeURIComponent(txt), true);//
             GM_openInTab("https://shopee.co.th/search?order=asc&page=0&sortBy=price&keyword=" + encodeURIComponent(txt), true);
             GM_openInTab("https://www.lazada.co.th/catalog/?_keyori=ss&from=search_history&page=1&sort=priceasc&q=" + encodeURIComponent(txt), true);
@@ -45,4 +39,6 @@ function getSelectionText() {
         }
     }
 searchSelectionText();
+//GM_registerMenuCommand("Search on Stores", searchSelectionText, "h");
 })();
+// ==/UserScript==
